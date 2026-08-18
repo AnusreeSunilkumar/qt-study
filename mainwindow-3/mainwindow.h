d318295a943f16cdbe3d0c9714e02df6e342f794
Anusree Sunilkumar <anusreepandath@gmail.com>
2026-08-18 13:30:04 +0900

    added new exercises

---
+++ mainwindow-3/mainwindow.h
@@ -0,0 +1,41 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QApplication>
+#include <QStyle>
+#include <QLabel>
+#include <QFileDialog>
+#include <QMessageBox>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow() override;
+
+private slots:
+    void on_actionOpen_triggered();
+
+    void on_actionZoom_In_triggered();
+
+    void on_actionZoom_Out_triggered();
+
+private:
+    Ui::MainWindow *ui;
+
+    QLabel *m_lblImage;
+    QString m_path;
+    QPixmap m_img;
+
+};
+#endif // MAINWINDOW_H
