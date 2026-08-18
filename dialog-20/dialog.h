32ff8c3526a63c78405f8ac809228ee6b0ea2351
Anusree Sunilkumar <anusreepandath@gmail.com>
2026-08-18 14:38:32 +0900

    added new exercises

---
+++ dialog-20/dialog.h
@@ -0,0 +1,39 @@
+#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <QDialog>
+#include <QPushButton>
+#include <QMessageBox>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Dialog;
+}
+QT_END_NAMESPACE
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Dialog(QWidget *parent = nullptr);
+    ~Dialog() override;
+
+private slots:
+    void on_buttonBox_accepted();
+
+    void on_buttonBox_rejected();
+
+    void next();
+    void back();
+
+private:
+    Ui::Dialog *ui;
+
+    QPushButton *btnNext;
+    QPushButton *btnBack;
+
+    void init();
+    void checkButtons();
+};
+#endif // DIALOG_H
